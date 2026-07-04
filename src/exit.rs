c6fe3b2cc73aaaeb893a45a212666045a9eef88c
cyberworrier8088 <cyberworrier39@gmail.com>
2026-07-04 12:00:38 +0530

    feat: add escape key to close game

---
+++ src/exit.rs
@@ -0,0 +1,13 @@
+use bevy::app::AppExit;
+use bevy::prelude::*;
+
+
+
+pub fn close_on_escape(
+    keyboard: Res<ButtonInput<KeyCode>>,
+    mut exit: MessageWriter<AppExit>,
+) {
+    if keyboard.just_pressed(KeyCode::Escape) {
+        exit.write(AppExit::Success);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@
 
 mod player;
 mod world;
+mod exit;
 
 use bevy::prelude::*;
 
 fn main() {
 
     App::new().add_plugins(DefaultPlugins).add_systems(Startup, (player::setup_player, world::setup_world, player::lock_cursor,))
-    .add_systems(Update, (player::player_movement, player::mouse_look, player::detect_block, (player::apply_gravity, player::apply_velocity, player::ground_collision,).chain()))
+    .add_systems(Update, (player::player_movement, player::mouse_look, player::detect_block, exit::close_on_escape, (player::apply_gravity, player::apply_velocity, player::ground_collision,).chain()))
     .run();
 }
