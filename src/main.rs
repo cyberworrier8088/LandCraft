0dce14c72e99242ab136428a90dcd4dfd60d6909
cyberworrier8088 <cyberworrier39@gmail.com>
2026-07-05 13:07:08 +0530

    feat: add centered crosshair UI

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@
 mod player;
 mod world;
 mod exit;
+mod ui;
 
 use bevy::prelude::*;
 
 fn main() {
 
-    App::new().add_plugins(DefaultPlugins).add_systems(Startup, (player::setup_player, world::setup_world, player::lock_cursor,))
+    App::new().add_plugins(DefaultPlugins).add_systems(Startup, (player::setup_player, world::setup_world, player::lock_cursor, ui::setup_crosshair,))
     .add_systems(Update, (player::player_movement, player::mouse_look, player::detect_block, exit::close_on_escape, (player::apply_gravity, player::apply_velocity, player::ground_collision,).chain()))
     .run();
 }
---
+++ src/ui.rs
@@ -0,0 +1,33 @@
+use bevy::prelude::*;
+
+
+
+// set up crosshair
+pub fn setup_crosshair(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    commands
+        .spawn(Node {
+            position_type: PositionType::Absolute,
+
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+
+            ..default()
+        })
+        .with_children(|parent| {
+            parent.spawn((
+                ImageNode::new(asset_server.load("crosshair.png")),
+                Node {
+                    width: Val::Px(24.0),
+                    height: Val::Px(24.0),
+
+                    ..default()
+                },
+            ));
+        });
+}
